5c3595ec0ed5a4ef648f9bd2accf2c4d12c5f629
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-20 08:31:08 +0300

    Реализовал ловлю шариков и обновление рекорда

---
+++ assets/scripts/CScore.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+
+public class CScore : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text;
+    private int score = 0;
+
+    private void Awake()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Start()
+    {
+        text.text = score.ToString();
+    }
+
+    public void Increment()
+    {
+        score++;
+        text.text = score.ToString();
+    }
+}
